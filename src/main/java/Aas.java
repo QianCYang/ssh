6e7c1b9e106770fcaa353e0fbffff2d170e9088c
QianCYang <love654176452@qq.com>
2018-11-28 17:39:19 +0800

    333

--- src/main/java/Aas.java
+++ src/main/java/Aas.java
@@ -1,2 +1,3 @@
 public class Aas {
+    private  int a;
 }
--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -3,8 +3,6 @@
  */
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println(111);
-        System.out.println(222);
 
     }
 }
