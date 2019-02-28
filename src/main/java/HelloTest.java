a047b9002954679e64557a1bb876648aa71244b1
QianCYang <love654176452@qq.com>
2019-02-28 14:20:34 +0800

    333

--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -1,8 +1,14 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by 转机丶 on 2018/7/23.
  */
 public class HelloTest {
     public static void main(String[] args) {
-
+        Map<String, Double> map = new LinkedHashMap<>();
+        map.containsKey("as");
+        Double ad = map.get("ad");
+        System.out.println(ad);
     }
 }
